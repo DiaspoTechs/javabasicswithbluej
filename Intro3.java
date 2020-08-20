2686354987cbcdce12d07817e0ae0ea0b6622963
TrishGatsi <69991803+TrishGatsi@users.noreply.github.com>
2020-08-20 18:00:33 -0400

    Committed Java Basics with BlueJ files

---
+++ Intro3.java
@@ -0,0 +1,18 @@
+/**
+ * Numbers and Strings
+ *
+ * @author Trish Gatsi
+ * @version August 18 2020
+ */
+public class Intro3
+{
+    /**The main function initiates the execution of this program.*/
+    public static void main()
+    {
+        System.out.println("2 + 4"); 
+         System.out.println(2 +  4 + 5);  
+          System.out.println(2 +  3 * 5); 
+           System.out.println((2 +  3) * 5);
+                 
+    }
+}
